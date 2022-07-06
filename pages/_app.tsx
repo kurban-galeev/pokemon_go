b51a94542a418eb530d718e8648813200c414f0b
kurban-galeev <kurban.galeev@mail.ru>
2022-07-06 16:17:15 +0400

    add typescript and styled component

---
+++ pages/_app.tsx
@@ -0,0 +1,40 @@
+import { JSXElementConstructor, ReactElement } from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+interface AppProps {
+  Component: () => ReactElement
+  pageProps: ReactElement<any, string | JSXElementConstructor<any>>
+
+}
+const theme = {
+  colors: {
+    powderWhite: "#FFFDF9",
+    persianGreen: "#06B49A",
+    lightBlue: "#AFDBD2",
+    onyx: "#36313D"
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em"
+  }
+};
+
+export default function App({ Component, pageProps }: AppProps ) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
